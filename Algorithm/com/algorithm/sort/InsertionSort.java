ae76143ca55081ee62617ff03725377886cce11d
Amit Kadmllu <akadmllu@gmail.com>
2025-03-04 10:18:19 +0530

    Update InsertionSort.java

--- Algorithm/com/algorithm/sort/InsertionSort.java
+++ Algorithm/com/algorithm/sort/InsertionSort.java
@@ -17,20 +17,59 @@ public class InsertionSort
 		System.out.println();
 	}
 	
-	private static void swap(int[] arr, int i, int j) 
-	{
-		if(i != j)
-		{
-			int temp = arr[i];
-			arr[i] = arr[j];
-			arr[j] = temp;	
-		}	
-	}
-	
 	private static void insertionSort(int[] arr)
 	{
-		
+		for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            // Move elements of arr[0..i-1], that are greater than key, to one position ahead of their current position
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
 	}
+	
+	// Function to perform optimized insertion sort
+	private static void optimizedInsertionSort(int[] arr) 
+	{
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            // Find the correct position of key using binary search
+            int pos = binarySearch(arr, key, 0, j);
+
+            // Move elements of arr[0..i-1], that are greater than key, to one position ahead of their current position
+            while (j >= pos) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    // Function to perform binary search
+	private static int binarySearch(int[] arr, int key, int low, int high) 
+	{
+        if (high <= low) {
+            return (key > arr[low]) ? (low + 1) : low;
+        }
+
+        int mid = (low + high) / 2;
+
+        if (key == arr[mid]) {
+            return mid + 1;
+        }
+
+        if (key > arr[mid]) {
+            return binarySearch(arr, key, mid + 1, high);
+        }
+
+        return binarySearch(arr, key, low, mid - 1);
+    }
 	
 	public static void main(String[] args) 
 	{
@@ -41,7 +80,17 @@ public class InsertionSort
 		
 		insertionSort(arr);
 		
-		System.out.print("sorted array : ");
+		System.out.print("sorted array after insertion sort : ");
 		printArray(arr);
+		
+		int[] arr2 = {6,8,12,1,14,17,5,3,29,25,4,5};
+		
+		System.out.print("unsorted array : ");
+		printArray(arr2);
+		
+		optimizedInsertionSort(arr2);
+		
+		System.out.print("sorted array after optimized insertion sort : ");
+		printArray(arr2);
 	}
 }
