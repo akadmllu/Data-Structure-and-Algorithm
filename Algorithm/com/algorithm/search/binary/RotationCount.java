0b2ab44e9022c7eef5ba416eaf8f28c2d411e3fe
Amit Kadmllu <akadmllu@gmail.com>
2025-03-18 13:37:35 +0530

    updated rotation count

--- Algorithm/com/algorithm/search/binary/RotationCount.java
+++ Algorithm/com/algorithm/search/binary/RotationCount.java
@@ -14,15 +14,13 @@ public class RotationCount {
 			
 			//Check if element (mid+1) is minimum 
 			//{3, 4, 5, 1, 2} 
-			//mid : 5 
-			//mid + 1 : 1
+			//mid : 5 , mid + 1 : 1
 			if(arr[mid] > arr[mid + 1])
 				return mid + 1;
 			
 			//Check if mid itself is minimum element
 			//{3, 4, 5, 1, 2} 
-			//mid : 1
-			//mid - 1 : 5
+			//mid : 1 , mid - 1 : 5
 			if(arr[mid] < arr[mid - 1])
 				return mid;
 			
@@ -36,11 +34,11 @@ public class RotationCount {
 			}
 		}
 		
-		return -1;
+		return 0;
 	}
 	
 	public static void main(String[] args) {
-		int[] arr = { 3, 3, 4, 5, 6, 6, 7, 0, 0, 1, 1, 2, 2, 2 , 3 };
+		int[] arr = { 1,2,3,4,5,6,7 };
 		System.out.println("Rotation Count : " + countRotations(arr));
 	}
 
